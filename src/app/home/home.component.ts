109dba02ce637b8ec1155b295665028e450bdc50
GemmaLleros <gemma.caballero.serra@gmail.com>
2023-03-27 20:07:28 +0200

    Exercise-3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,38 +1,50 @@
+
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup,FormControl } from '@angular/forms';
+import { BudgetService } from '../services/budget.service';
+
+interface service {
+  name: string;
+  price: number;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  form: FormGroup;
+  services: any[] = [
+    { name: 'Una página web', price: 500, selected: false },
+    { name: 'Una consultoría SEO', price: 300, selected: false },
+    { name: 'Una campaña de Google Ads', price: 200, selected: false },
+  ];
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      web: false,
-      seo: false,
-      ads: false
+  myForm: FormGroup;
+  totalpanel = this.BudgetService.totalPrice;
+  
+  constructor(private formBuilder: FormBuilder, private BudgetService: BudgetService) {
+    this.myForm = this.formBuilder.group({
+      selectedService0: new FormControl(false),
+      selectedService1: new FormControl(false),
+      selectedService2: new FormControl(false)
     });
   }
-  get total() {
-    let price = 0;
+  get selectedService0() {
+    return this.myForm.get('selectedService0');
+  }
 
-    if (this.form.value.web) {
-      price += 500;
+  get totalSum() {
+    let sum = this.services.reduce((acc, curr) => {
+      return curr.selected ? acc + curr.price : acc;
+    }, 0);
+
+    if (this.services[0].selected) {
+      sum += this.BudgetService.totalPrice;
     }
 
-    if (this.form.value.seo) {
-      price += 300;
-    }
-
-    if (this.form.value.ads) {
-      price += 200;
-    }
-
-    return price;
+    return sum;
   }
 }
-
 
--- src/app/panell/panell.component.ts
+++ src/app/panell/panell.component.ts
@@ -20,6 +20,28 @@ export class PanellComponent {
       });
       this.pages =  this.form.get('pages') as AbstractControl;
       this.languages =  this.form.get('languages') as AbstractControl;
-      
+
+    // Agregar el listener para los cambios de valor en el formulario
+    this.form.valueChanges.subscribe(() => {
+      this.getValue();
+    });
+  }
+
+  sumar(control: AbstractControl) {
+    const currentValue = control.value;
+    control.setValue(currentValue + 1);
+  }
+
+  restar(control: AbstractControl) {
+    const currentValue = control.value;
+    if (currentValue > 1) {
+      control.setValue(currentValue - 1);
+    }
+  }
+  
+  getValue() {
+    const pages = this.pages.value;
+    const languages = this.languages.value;
+    this.budgetService.calculateTotal(pages, languages);
   }
 }
